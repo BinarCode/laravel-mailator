5947786f156be8e33111a034171afda25f338ce5
Lupacescu Eduard <lupacescueduard@yahoo.com>
2020-02-07 09:40:42 +0000

    Apply fixes from StyleCI

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,17 +2,16 @@
 
 namespace Binarcode\LaravelMailator\Tests;
 
-use Orchestra\Testbench\TestCase;
 use Binarcode\LaravelMailator\LaravelMailatorServiceProvider;
+use Orchestra\Testbench\TestCase;
 
 class ExampleTest extends TestCase
 {
-
     protected function getPackageProviders($app)
     {
         return [LaravelMailatorServiceProvider::class];
     }
-    
+
     /** @test */
     public function true_is_true()
     {
