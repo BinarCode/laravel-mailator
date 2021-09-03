06c3e8b965bb0f1b7ccb1485e3c395612117d90c
binaryk <binaryk@users.noreply.github.com>
2021-09-03 09:25:52 +0000

    Fix styling

--- src/Models/MailatorSchedule.php
+++ src/Models/MailatorSchedule.php
@@ -25,7 +25,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use Opis\Closure\SerializableClosure;
