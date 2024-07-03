96795fef887ccf8aff7dfa985517f1149116e43c
Lupacescu Eduard <eduard.lupacescu@binarcode.com>
2024-07-03 10:24:59 +0300

    fix: fixing minutes in days constant (#2587)

--- src/Support/ConverterEnum.php
+++ src/Support/ConverterEnum.php
@@ -5,7 +5,7 @@ namespace Binarcode\LaravelMailator\Support;
 class ConverterEnum
 {
     public const MINUTES_IN_HOUR = 60;
-    public const MINUTES_IN_DAY = 60 * 60;
+    public const MINUTES_IN_DAY = 60 * 24;
     public const MINUTES_IN_WEEK = 168 * 60;
     public const HOURS_IN_DAY = 24;
     public const HOURS_IN_WEEK = 168;
