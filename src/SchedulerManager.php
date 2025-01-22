7477a342f472396a28a5007d521bdca5e782139c
Eduard Lupacescu <lupacescueduard@gmail.com>
2025-01-22 08:37:38 +0200

    fix: fixing instance not initialized

--- src/SchedulerManager.php
+++ src/SchedulerManager.php
@@ -24,7 +24,7 @@ class SchedulerManager
 
     public function __destruct()
     {
-        if ($this->instance && ! $this->instance->wasRecentlyCreated) {
+        if (isset($this->instance) && $this->instance && ! $this->instance->wasRecentlyCreated) {
             $this->instance->save();
         }
     }
